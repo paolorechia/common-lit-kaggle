7dc5cedea51591c4b13d6c4caa2fa5fe0d6c4f08
Paolo Rechia <paolorechia@gmail.com>
2023-07-23 13:44:47 +0200

    Resovle path in run time

--- common_lit_kaggle/tables/table_summaries.py
+++ common_lit_kaggle/tables/table_summaries.py
@@ -4,15 +4,12 @@ from common_lit_kaggle.framework.table import TableReference
 from common_lit_kaggle.schemas import InputSummarySchema
 from common_lit_kaggle.settings import config
 
-INPUT_CSV = "summaries_train.csv"
-INPUT_CSV_FULL_PATH = pathlib.Path(config.DATA_INPUT_DIR, INPUT_CSV)
-
 
 class InputSummariesTable(TableReference):
     def __init__(self):
         super().__init__(
             name="input_summaries",
-            path=INPUT_CSV_FULL_PATH,
+            path=pathlib.Path(config.DATA_INPUT_DIR, "summaries_train.csv"),
             schema=InputSummarySchema,
             format="csv",
         )
