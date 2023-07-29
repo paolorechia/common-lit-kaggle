d0713d60f86a92255c76c63ae41cea2d66dfc228
Paolo Rechia <paolorechia@gmail.com>
2023-07-29 20:43:58 +0200

    Comment out possibly bad code

--- common_lit_kaggle/tasks/bart/task_create_unified_text_data.py
+++ common_lit_kaggle/tasks/bart/task_create_unified_text_data.py
@@ -7,7 +7,7 @@ from common_lit_kaggle.framework.task import Task
 from common_lit_kaggle.tables import UnifiedTextDataTable
 
 
-def add_unified_data(train_data: pl.DataFrame, labeled=True) -> pl.DataFrame:
+def add_unified_data(train_data: pl.DataFrame) -> pl.DataFrame:
     # Prepare a single string to train transformers
     unified_text_data = train_data.with_columns(
         pl.concat_str(
@@ -24,21 +24,21 @@ def add_unified_data(train_data: pl.DataFrame, labeled=True) -> pl.DataFrame:
         ).alias("unified_text")
     )
 
-    if labeled:
-        # If we want to train a GPT-like model, we can include the labels as part of the text
-        # Have the string ready, so later we can just concatenate together with the unified_text
-        unified_text_data = unified_text_data.with_columns(
-            pl.concat_str(
-                [
-                    pl.lit("\n\nGRADING SECTION"),
-                    pl.lit("\nWORDING: "),
-                    pl.col("wording"),
-                    pl.lit("\nCONTENT: "),
-                    pl.col("content"),
-                    pl.lit("\nEND_OF_TEXT\n"),
-                ]
-            ).alias("unified_labels")
-        )
+    # I think for now we should disable this block altogether
+    #     # If we want to train a GPT-like model, we can include the labels as part of the text
+    #     # Have the string ready, so later we can just concatenate together with the unified_text
+    #     unified_text_data = unified_text_data.with_columns(
+    #         pl.concat_str(
+    #             [
+    #                 pl.lit("\n\nGRADING SECTION"),
+    #                 pl.lit("\nWORDING: "),
+    #                 pl.col("wording"),
+    #                 pl.lit("\nCONTENT: "),
+    #                 pl.col("content"),
+    #                 pl.lit("\nEND_OF_TEXT\n"),
+    #             ]
+    #         ).alias("unified_labels")
+    #     )
     return unified_text_data
 
 
@@ -73,6 +73,6 @@ class CreateUnifiedTextTestDataTask(Task):
 class CreateUnifiedTextPredictionDataTask(Task):
     def run(self, context: Mapping[str, Any]) -> Mapping[str, Any]:
         prediction_input_data: pl.DataFrame = context["input_prediction_data"]
-        unified_text_data = add_unified_data(prediction_input_data, labeled=False)
+        unified_text_data = add_unified_data(prediction_input_data)
 
         return {"prediction_unified_text_data": unified_text_data}
